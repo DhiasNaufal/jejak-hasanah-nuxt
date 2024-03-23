d079eebf3d81b6dd1095a96fb4de0d0755135be8
Dhias Muhammad Naufal <91380628+DhiasNaufal@users.noreply.github.com>
2024-03-24 00:50:13 +0700

    Fix HMR bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,23 @@ export default {
   ],
   theme: {
     extend: {
+      presets: {
+        "my-heading": ["font-bold", "text-3xl", "text-blue-700"], // Example classes
+      },
       colors: {
+        JHui: {
+          50: "#eef3ff",
+          100: "#dfe9ff",
+          200: "#c6d4ff",
+          300: "#a3b8fe",
+          400: "#7e91fb",
+          500: "#606cf4",
+          600: "#4344e8",
+          700: "#3735cd",
+          800: "#2e2ea5",
+          900: "#2f318b",
+          950: "#1a1a4c",
+        },
         JH: {
           red: "#E60013",
           blue: "#2F318B",
