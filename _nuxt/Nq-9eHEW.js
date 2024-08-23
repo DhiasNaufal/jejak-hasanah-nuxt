dd31a2439dc51bdeba4efbb40d533f748a836f1b
DhiasNaufal <91380628+DhiasNaufal@users.noreply.github.com>
2024-08-23 04:12:14 +0000

    Build: (b7f0009) Dah bisa diliat

--- _nuxt/Nq-9eHEW.js
+++
@@ -1,1 +0,0 @@
-import{j as o,o as t,c as n,k as s}from"./ZS-miP3L.js";const c={},_={class:"font-bold text-xl"};function r(e,a){return t(),n("h2",_,[s(e.$slots,"default")])}const f=o(c,[["render",r]]);export{f as _};
